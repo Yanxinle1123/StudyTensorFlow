04bbb610b4dee6df103d3f112bcf8c4822859ccf
yanxinle <1020121123@qq.com>
2024-02-25 20:46:39 +0800

    增加训练次数

--- tensorflow1.py
+++ tensorflow1.py
@@ -22,7 +22,7 @@ model.compile(optimizer='adam',
               loss=loss_fn,
               metrics=['accuracy'])
 
-model.fit(x_train, y_train, epochs=5)
+model.fit(x_train, y_train, epochs=20)
 
 model.evaluate(x_test, y_test, verbose=2)
 
